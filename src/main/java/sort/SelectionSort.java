1523e8b0e88ebb1f3126cb7036594dcf7c5104ec
Reza Shakoori <rshakoori@irhythmtech.com>
2022-01-04 17:23:21 -0800

    Selection sort update

--- src/main/java/sort/SelectionSort.java
+++ src/main/java/sort/SelectionSort.java
@@ -7,31 +7,24 @@ final class SelectionSort {
     static final int[] ARR0 = { 8, 6, 2, 4, 5, 8 };
     static final int[] ARR1 = { 6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90, 12, 55, 82, 3, 29, 69, 13, 57, 73, 5 };
 
-    void selectionSort(int[] nums) {
+    void selectionSort(int[] A) {
 
-        int counter = 0;
-
-        for (int i = 0; i < nums.length-1; i++) {
+        for (int i = 0; i < A.length-1; i++) {
             int minIdx = i;
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[j] < nums[minIdx]) {
+            int temp = A[i];
+            for (int j = i+1; j < A.length; j++) {
+                if (A[j] < A[minIdx]) {
                     minIdx = j;
                 }
             }
-            int temp = nums[i];
-            nums[i] = nums[minIdx];
-            nums[minIdx] = temp;
-            counter++;
+            A[i] = A[minIdx];
+            A[minIdx] = temp;
         }
-
-        System.out.println(counter);
     }
 
     public static void main(String... args) {
-
         SelectionSort ss = new SelectionSort();
         ss.selectionSort(ARR1);
         System.out.println(Arrays.toString(ARR1));
-
     }
 }
