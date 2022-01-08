b9c86ad737c9514fe3913e96aed1182da7a2edcd
Reza Shakoori <rshakoori@irhythmtech.com>
2022-01-07 19:58:22 -0800

    Fibonacci updates

--- src/main/java/dp/Fibonacci.java
+++ src/main/java/dp/Fibonacci.java
@@ -1,18 +1,20 @@
 package dp;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 final class Fibonacci {
 
     // time: Ω(n), Θ(n), O(n); space: O(n)
     int[] fibonacciDynamic(int n) {
-        int[] result = new int[n + 1];
-        result[0] = 0;
-        result[1] = 1;
+        int[] fibs = new int[n + 1];
+        fibs[0] = 0;
+        fibs[1] = 1;
         for (int i = 2; i <= n; i++) {
-            result[i] = result[i - 1] + result[i - 2];
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
         }
-        return result;
+        return fibs;
     }
 
     // time: Ω(n), Θ(n), O(n); space: O(1)
@@ -34,11 +36,23 @@ final class Fibonacci {
         return fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1);
     }
 
+    // time: Ω(n), Θ(n), O(n); space: O(n)
+    int fibonacciRecursiveDynamic(int n) {
+        Map<Integer, Integer> cache = new HashMap<>();
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            cache.put(n, fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1));
+            return cache.get(n);
+        }
+    }
+
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
         System.out.println(Arrays.toString(f.fibonacciDynamic(10)));
         System.out.println(f.fibonacciIterative(8));
         System.out.println(f.fibonacciRecursive(8));
+        System.out.println(f.fibonacciRecursiveDynamic(8));
     }
 
 }
