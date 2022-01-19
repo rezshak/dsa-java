f9009d9ba431e5389830d4585a09a23a6ea22209
Reza Shakoori <rshakoori@irhythmtech.com>
2022-01-18 16:54:18 -0800

    Insertion sort update

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 final class InsertionSort {
 
-    static final int[] ARR0 = {1, 3, 2};
+    static final int[] ARR0 = {3, 2, 1};
     static final int[] ARR1 = {8, 6, 2, 4, 5, 8};
     static final int[] ARR2 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90, 12, 55, 82, 3, 29, 69, 13, 57, 73, 5};
     static final int[] ARR3 = {23, 7, 29, 79, 12, 41, 57, 72, 45, 82, 51, 10, 40, 85, 72, 13, 50, 90, 100, 6, 39, 100};
@@ -13,7 +13,7 @@ final class InsertionSort {
         for (int i = 1; i < A.length; i++) {
             int key = A[i];
             int j = i - 1;
-            while (j >= 0 && key < A[j]) {
+            while (j >= 0 && A[j] > key) {
                 A[j + 1] = A[j];
                 j--;
             }
