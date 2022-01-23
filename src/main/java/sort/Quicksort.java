e232798130bb0de63202fcdd136cba7fb0c49267
Reza Shakoori <rshakoori@irhythmtech.com>
2022-01-23 13:33:49 -0800

    Quicksort updates

--- src/main/java/sort/Quicksort.java
+++ src/main/java/sort/Quicksort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 final class Quicksort {
 
     static final int[] ARR0 = {8};
-    static final int[] ARR1 = {8, 6, 2};
+    static final int[] ARR1 = {8, 6};
     static final int[] ARR2 = {8, 6, 2, 4, 5, 8, 9};
     static final int[] ARR3 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90};
     static final int[] ARR4 = {36, 72, 98, 98, 9, 60, 18, 60, 62, 74, 45, 62, 15, 55, 53, 100, 15, 21, 71, 48, 21, 43};
@@ -35,7 +35,7 @@ final class Quicksort {
 
         swap(A, i + 1, right);
 
-        return (i + 1);
+        return i + 1;
     }
 
     int hoarePartition(int[] A, int left, int right) {
@@ -65,8 +65,8 @@ final class Quicksort {
 
     public static void main(String... args) {
         Quicksort qs = new Quicksort();
-        qs.quicksort(ARR3, 0, ARR3.length - 1);
-        System.out.println(Arrays.toString(ARR3));
+        qs.quicksort(ARR1, 0, ARR1.length - 1);
+        System.out.println(Arrays.toString(ARR1));
     }
 
     private static void swap(int[] A, int i, int j) {
