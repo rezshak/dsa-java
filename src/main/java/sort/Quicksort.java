09b76eb41d364e7a3291989fd863ea776ad964fb
Reza Shakoori <rshakoori@irhythmtech.com>
2022-01-23 10:14:16 -0800

    Quicksort updates

--- src/main/java/sort/Quicksort.java
+++ src/main/java/sort/Quicksort.java
@@ -10,24 +10,44 @@ final class Quicksort {
     static final int[] ARR3 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90};
     static final int[] ARR4 = {36, 72, 98, 98, 9, 60, 18, 60, 62, 74, 45, 62, 15, 55, 53, 100, 15, 21, 71, 48, 21, 43};
 
-    void quicksort(int[] A, int lo, int hi) {
+    void quicksort(int[] A, int left, int right) {
 
-        if (lo < 0 || lo >= hi) {
+        if (left < 0 || left >= right) {
             return;
         }
 
-        int pi = hoarePartition(A, lo, hi);
-        quicksort(A, lo, pi - 1);
-        quicksort(A, pi + 1, hi);
+        int pivot = lomutoPartition(A, left, right);
+        quicksort(A, left, pivot - 1);
+        quicksort(A, pivot + 1, right);
     }
 
-    // Hoare partition scheme
-    int hoarePartition(int[] A, int lo, int hi) {
+    int lomutoPartition(int[] A, int left, int right) {
 
-        int mid = (hi + lo) / 2;
+        int pivot = A[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (A[j] <= pivot) {
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+
+        int temp = A[i + 1];
+        A[i + 1] = A[right];
+        A[right] = temp;
+
+        return (i + 1);
+    }
+
+    int hoarePartition(int[] A, int left, int right) {
+
+        int mid = (right + left) / 2;
         int pivot = A[mid];
-        int i = lo - 1;
-        int j = hi + 1;
+        int i = left - 1;
+        int j = right + 1;
 
         while (true) {
 
@@ -47,37 +67,14 @@ final class Quicksort {
         }
     }
 
-    // Lomuto partition scheme
-    int lomutoPartition(int[] A, int lo, int hi) {
-
-        int pivot = A[hi];
-        int i = lo - 1;
-
-        for (int j = lo; j < hi; j++) {
-            if (A[j] <= pivot) {
-                i++;
-                int temp = A[i];
-                A[i] = A[j];
-                A[j] = temp;
-            }
-        }
-
-        int temp = A[i + 1];
-        A[i + 1] = A[hi];
-        A[hi] = temp;
-
-        return (i + 1);
-    }
-
     public static void main(String... args) {
-
         Quicksort qs = new Quicksort();
-        qs.quicksort(ARR1, 0, ARR1.length - 1);
-        System.out.println(Arrays.toString(ARR1));
-
+        qs.quicksort(ARR3, 0, ARR3.length - 1);
+        System.out.println(Arrays.toString(ARR3));
     }
 
     private static void swap(int[] A, int i, int j) {
+        if (i == j) return;
         int temp = A[i];
         A[i] = A[j];
         A[j] = temp;
