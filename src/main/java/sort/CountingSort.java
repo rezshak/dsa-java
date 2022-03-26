69936fd18513711cf708a1dfd17a0c61216e02ff
Reza Shakoori <reza.shakoori@cornell.edu>
2022-03-25 21:31:07 -0700

    Counting sort updates

--- src/main/java/sort/CountingSort.java
+++ src/main/java/sort/CountingSort.java
@@ -20,24 +20,21 @@ final class CountingSort {
     }
 
     void countingSortLegacy(int[] A) {
-
-        int len = A.length;
         int max = Arrays.stream(A).max().getAsInt();
-
         int[] counts = new int[max + 1];
         for (int n : A) counts[n]++;
 
         for (int i = 1; i <= max; i++) counts[i] += counts[i - 1];
 
-        int[] sorted = new int[len];
-        for (int i = len - 1; i >= 0; i--) {
+        int[] sorted = new int[A.length];
+        for (int i = sorted.length - 1; i >= 0; i--) {
             int origVal = A[i];
             int origValIdx = counts[A[i]] - 1;
             sorted[origValIdx] = origVal;
             counts[origVal]--;
         }
 
-        System.arraycopy(sorted, 0, A, 0, len);
+        System.arraycopy(sorted, 0, A, 0, A.length);
     }
 
     public static void main(String[] args) {
