8af8aeed771c4102d6e51848b9e7094fb3de947f
Reza Shakoori <rshakoori@irhythmtech.com>
2022-03-25 18:55:15 -0700

    Counting sort update

--- src/main/java/sort/CountingSort.java
+++ src/main/java/sort/CountingSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 final class CountingSort {
 
     static final int[] ARR0 = {1, 3, 2};
-    static final int[] ARR1 = {8, 6, 2, 4, 5, 8};
+    static final int[] ARR1 = {7, 6, 2, 4, 3, 7};
 
     void countingSort(int[] A) {
 
@@ -28,10 +28,20 @@ final class CountingSort {
         System.arraycopy(sorted, 0, A, 0, len);
     }
 
+    public void countingSort2(int[] A, int max) {
+        int[] count = new int[max + 1];
+        for (int n : A) count[n]++;
+
+        int k = 0;
+        for (int i = 0; i < count.length; i++)
+            for (int j = 0; j < count[i]; j++)
+                A[k++] = i;
+    }
+
     public static void main(String[] args) {
         CountingSort cs = new CountingSort();
         System.out.println("Original: " + Arrays.toString(ARR1));
-        cs.countingSort(ARR1);
+        cs.countingSort2(ARR1, 7);
         System.out.println("Sorted: " + Arrays.toString(ARR1));
     }
 
