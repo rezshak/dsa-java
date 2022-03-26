c57ad8a57023b2caad240bd8932ae58c81dc2657
Reza Shakoori <reza.shakoori@cornell.edu>
2022-03-25 20:57:47 -0700

    Counting sort updates

--- src/main/java/sort/CountingSort.java
+++ src/main/java/sort/CountingSort.java
@@ -4,44 +4,46 @@ import java.util.Arrays;
 
 final class CountingSort {
 
-    static final int[] ARR0 = {1, 3, 2};
-    static final int[] ARR1 = {7, 6, 2, 4, 3, 7};
+    static final int[] ARR0 = { 1, 3, 2 };
+    static final int[] ARR1 = { 2, 5, 3, 1, 5 };
 
-    void countingSort(int[] A) {
+    public void countingSort(int[] A) {
+        int max = Arrays.stream(A).max().getAsInt();
+        int[] counts = new int[max + 1];
+        for (int n : A) counts[n]++;
+        int k = 0;
+        for (int i = 0; i < counts.length; i++) {
+            for (int j = 0; j < counts[i]; j++) {
+                A[k++] = i;
+            }
+        }
+    }
+
+    void countingSortLegacy(int[] A) {
 
         int len = A.length;
         int max = Arrays.stream(A).max().getAsInt();
 
-        int[] count = new int[max + 1];
-        for (int n : A) count[n]++;
+        int[] counts = new int[max + 1];
+        for (int n : A) counts[n]++;
 
-        for (int i = 1; i <= max; i++) count[i] += count[i - 1];
+        for (int i = 1; i <= max; i++) counts[i] += counts[i - 1];
 
         int[] sorted = new int[len];
         for (int i = len - 1; i >= 0; i--) {
             int origVal = A[i];
-            int origValIdx = count[A[i]] - 1;
+            int origValIdx = counts[A[i]] - 1;
             sorted[origValIdx] = origVal;
-            count[origVal]--;
+            counts[origVal]--;
         }
 
         System.arraycopy(sorted, 0, A, 0, len);
     }
 
-    public void countingSort2(int[] A, int max) {
-        int[] count = new int[max + 1];
-        for (int n : A) count[n]++;
-
-        int k = 0;
-        for (int i = 0; i < count.length; i++)
-            for (int j = 0; j < count[i]; j++)
-                A[k++] = i;
-    }
-
     public static void main(String[] args) {
         CountingSort cs = new CountingSort();
         System.out.println("Original: " + Arrays.toString(ARR1));
-        cs.countingSort2(ARR1, 7);
+        cs.countingSort(ARR1);
         System.out.println("Sorted: " + Arrays.toString(ARR1));
     }
 
