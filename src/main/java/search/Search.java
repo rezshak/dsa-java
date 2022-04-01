584fc3597cadb38dd7b22449daf71f44c73e59fb
Reza Shakoori <rshakoori@irhythmtech.com>
2022-03-31 18:35:36 -0700

    Adding search

---
+++ src/main/java/search/Search.java
@@ -0,0 +1,54 @@
+package search;
+
+import java.util.Arrays;
+
+final class Search {
+
+    final static int[] ARR1 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90};
+
+    int linearSearch(int[] A, int target) {
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == target) return i;
+        }
+        return -1;
+    }
+
+    int binarySearch(int[] A, int target) {
+        Arrays.sort(A);
+        return binarySearch(A, 0, A.length - 1, target);
+    }
+
+    int binarySearch(int[] A, int left, int right, int target) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (A[mid] == target) return mid;
+        else if (A[mid] > target) return binarySearch(A, left, mid - 1, target);
+        else return binarySearch(A, mid + 1, right, target);
+    }
+
+    int binarySearchIterative(int[] A, int target) {
+        Arrays.sort(A);
+        int left = 0, right = A.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] == target) return mid;
+            else if (A[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Search search = new Search();
+        System.out.println(search.linearSearch(ARR1, 90));
+        System.out.println(search.linearSearch(ARR1, 91));
+
+        System.out.println(search.binarySearch(ARR1, 90));
+        System.out.println(search.binarySearch(ARR1, 91));
+
+        System.out.println(search.binarySearchIterative(ARR1, 90));
+        System.out.println(search.binarySearchIterative(ARR1, 91));
+
+    }
+
+}
