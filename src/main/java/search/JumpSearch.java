937b89f210a8ad5e23fc2fba161dc37a7c7dd56a
Reza Shakoori <rshakoori@irhythmtech.com>
2022-04-01 11:32:44 -0700

    Add jump search

---
+++ src/main/java/search/JumpSearch.java
@@ -0,0 +1,36 @@
+package search;
+
+final class JumpSearch {
+
+    int jumpSearch(int[] A, int target) {
+
+        int len = A.length, blockSize = (int) Math.sqrt(len);
+        int start = 0, next = blockSize;
+
+        while (start < len && A[next - 1] < target) {
+            start = next;
+            next += blockSize;
+            if (next > len) next = len;
+        }
+
+        for (int i = start; i < next; i++) {
+            if (A[i] == target) return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        JumpSearch js = new JumpSearch();
+
+        final int[] arr1 = {1, 3, 7, 8, 9};
+
+        System.out.println("-- Jump Search");
+        System.out.println(js.jumpSearch(arr1, 1));
+        System.out.println(js.jumpSearch(arr1, 9));
+        System.out.println(js.jumpSearch(arr1, 99));
+
+    }
+
+}
