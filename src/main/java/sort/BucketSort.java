2e02ac4a16a60474ae8ed2f9f822d1d223d1c216
Reza Shakoori <reza.shakoori@cornell.edu>
2022-04-21 18:02:06 -0700

    Bucket sort tweak

--- src/main/java/sort/BucketSort.java
+++ src/main/java/sort/BucketSort.java
@@ -7,37 +7,33 @@ final class BucketSort {
     static final int[] ARR1 = {8, 6, 2, 4, 5, 8, 9};
     static final int[] ARR2 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90};
 
-    void bucketSort(int[] A) {
-        int numberOfBuckets = A.length / 2;
-        List<List<Integer>> buckets = new ArrayList<>();
-        for (int i = 0; i < numberOfBuckets; i++) {
-            buckets.add(new ArrayList<>());
-        }
-
-        for (int n : A) {
-            int bucketIndex = n / numberOfBuckets;
-            if (bucketIndex >= buckets.size()) bucketIndex = buckets.size() - 1;
-            buckets.get(bucketIndex).add(n);
-        }
-
-        for (int i = 0; i < numberOfBuckets; i++) {
-            List<Integer> bucket = buckets.get(i);
+    void bucketSort(int[] A, int numberOfBuckets) {
+        var i = 0;
+        for (var bucket : createBuckets(A, numberOfBuckets)) {
             Collections.sort(bucket);
+            for (var item : bucket)
+                A[i++] = item;
+        }
+    }
+
+    List<List<Integer>> createBuckets(int[] A, int numberOfBuckets) {
+        List<List<Integer>> buckets = new ArrayList<>();
+        for (var i = 0; i < numberOfBuckets; i++)
+            buckets.add(new ArrayList<>());
+
+        for (var item : A) {
+            int bucketIndex = item / numberOfBuckets;
+            if (bucketIndex > numberOfBuckets) bucketIndex = numberOfBuckets - 1;
+            buckets.get(bucketIndex).add(item);
         }
 
-        int k = 0;
-        for (int i = 0; i < numberOfBuckets; i++) {
-            List<Integer> bucket = buckets.get(i);
-            for (int n : bucket) {
-                A[k++] = n;
-            }
-        }
+        return buckets;
     }
 
     public static void main(String[] args) {
         BucketSort bs = new BucketSort();
         System.out.println("Original: " + Arrays.toString(ARR2));
-        bs.bucketSort(ARR2);
+        bs.bucketSort(ARR2, 3);
         System.out.println("Sorted: " + Arrays.toString(ARR2));
     }
 
