aa2c4db978d88d494e0d753ec4492c114383a68c
Reza Shakoori <reza.shakoori@cornell.edu>
2022-05-01 16:15:48 -0700

    Fibs tweak

--- src/main/java/dp/Fibonacci.java
+++ src/main/java/dp/Fibonacci.java
@@ -19,32 +19,27 @@ final class Fibonacci {
 
     // Time: Ω(n), Θ(n), O(n); Space: O(1)
     int fibonacciIterative(int n) {
-        int a = 0, b = 1;
+        int fib = 0, a = 0, b = 1;
         for (int i = 2; i <= n; i++) {
-            int temp = a + b;
+            fib = a + b;
             a = b;
-            b = temp;
+            b = fib;
         }
-        return b;
+        return fib;
     }
 
     // Time: Ω(2^n), Θ(2^n), O(2^n); Space: O(n)
     int fibonacciRecursive(int n) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
+        if (n == 0 || n == 1) return n;
         return fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1);
     }
 
     // Time: Ω(n), Θ(n), O(n); Space: O(n)
     int fibonacciRecursiveDynamic(int n) {
         Map<Integer, Integer> cache = new HashMap<>();
-        if (n == 0 || n == 1) {
-            return n;
-        } else {
-            cache.put(n, fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1));
-            return cache.get(n);
-        }
+        if (n == 0 || n == 1) return n;
+        cache.put(n, fibonacciRecursive(n - 2) + fibonacciRecursive(n - 1));
+        return cache.get(n);
     }
 
     // Time: Ω(n), Θ(n), O(n); Space: O(n)
