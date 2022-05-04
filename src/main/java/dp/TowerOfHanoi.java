f0ad4baedaab6dab10134c265707e49bd9fbca67
Reza Shakoori <reza.shakoori@cornell.edu>
2022-05-03 18:19:10 -0700

    Adding tower of hanoi

---
+++ src/main/java/dp/TowerOfHanoi.java
@@ -0,0 +1,24 @@
+package dp;
+
+public class TowerOfHanoi {
+
+    static int towerOfHanoi(int discs) {
+        return (int) (Math.pow(2, discs) - 1);
+    }
+
+    static void towerOfHanoiRecursive(int discs, String src, String dst, String aux) {
+        if (discs == 1)
+            System.out.println("> Moving disc from " + src + " to " + dst);
+        else {
+            towerOfHanoiRecursive(discs - 1, src, aux, dst);
+            System.out.println(">> Moving disc from " + src + " to " + aux);
+            towerOfHanoiRecursive(discs - 1, aux, src, dst);
+        }
+    }
+
+    public static void main(String[] args) {
+        towerOfHanoiRecursive(3, "A", "B", "C");
+        System.out.println(towerOfHanoi(3));
+    }
+
+}
