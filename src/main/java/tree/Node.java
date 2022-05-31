df368be3e917b72d4630e38f0538ce926f89a265
Reza Shakoori <reza@RezM1Pro.local>
2022-05-31 12:18:58 -0700

    Adding tree node

---
+++ src/main/java/tree/Node.java
@@ -0,0 +1,16 @@
+package tree;
+
+final class Node {
+    int val;
+    Node left, right;
+
+    Node(int val) {
+        this.val = val;
+        left = right = null;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
