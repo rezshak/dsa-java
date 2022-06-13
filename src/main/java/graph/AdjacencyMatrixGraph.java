70cee27e54ee36b114cc033595d784b8aca870ee
Reza Shakoori <reza.shakoori@cornell.edu>
2022-06-12 20:04:17 -0700

    Adding adjacency matrix graph

---
+++ src/main/java/graph/AdjacencyMatrixGraph.java
@@ -0,0 +1,48 @@
+package graph;
+
+final class AdjacencyMatrixGraph {
+
+    final int numVertices;
+    final boolean[][] adjMatrix;
+
+    AdjacencyMatrixGraph(int numVertices) {
+        this.numVertices = numVertices;
+        adjMatrix = new boolean[numVertices][numVertices];
+    }
+
+    void addEdge(int i, int j) {
+        adjMatrix[i][j] = true;
+        adjMatrix[j][i] = true;
+    }
+
+    void removeEdge(int i, int j) {
+        adjMatrix[i][j] = true;
+        adjMatrix[j][i] = true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < numVertices; i++) {
+            sb.append(i).append(" ");
+            for (boolean j : adjMatrix[i]) {
+                sb.append(j ? 1 : 0).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AdjacencyMatrixGraph g = new AdjacencyMatrixGraph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        System.out.println(g);
+        g.removeEdge(1, 2);
+        System.out.println(g);
+    }
+
+}
