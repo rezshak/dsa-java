f19edb46dd62794253160a36c0138b58e0ee761f
Reza Shakoori <reza.shakoori@cornell.edu>
2022-06-14 14:30:33 -0700

    Adding prime factors

---
+++ src/main/java/numeric/PrimeFactors.java
@@ -0,0 +1,24 @@
+package numeric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class PrimeFactors {
+
+    static List<Integer> primeFactors(int num) {
+        List<Integer> primeFactors = new ArrayList<>();
+        for (int i = 2; i< num; i++) {
+            while(num % i == 0) {
+                primeFactors.add(i);
+                num /= i;
+            }
+        }
+        if (num > 2) primeFactors.add(num);
+        return primeFactors;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(primeFactors(24));
+    }
+
+}
