524e50e8c6283ad207710ad2388c6ce5da720a61
Reza Shakoori <reza.shakoori@cornell.edu>
2022-06-21 21:31:33 -0700

    Tower of Hanoi tweaks

--- src/main/java/recursion/TowerOfHanoi.java
+++ src/main/java/recursion/TowerOfHanoi.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import static java.lang.String.format;
+
 public class TowerOfHanoi {
 
     static int towerOfHanoi(int discs) {
@@ -7,13 +9,23 @@ public class TowerOfHanoi {
     }
 
     static void towerOfHanoiRecursive(int discs, String src, String dst, String aux) {
-        if (discs == 1)
-            System.out.println("> Moving disc from " + src + " to " + dst);
+        if (discs == 1) shift(1, src, dst);
         else {
             towerOfHanoiRecursive(discs - 1, src, aux, dst);
-            System.out.println(">> Moving disc from " + src + " to " + aux);
-            towerOfHanoiRecursive(discs - 1, aux, src, dst);
+            shift(discs, src, dst);
+            towerOfHanoiRecursive(discs - 1, aux, dst, src);
         }
+    }
+
+    static void towerOfHanoiRecursive2(int discs, String src, String dst, String aux) {
+        if (discs == 0) return;
+        towerOfHanoiRecursive2(discs - 1, src, aux, dst);
+        shift(discs - 1, src, dst);
+        towerOfHanoiRecursive2(discs - 1, aux, dst, src);
+    }
+
+    private static void shift(int disc, String src, String dst) {
+        System.out.printf("> Moving disc '%d' from '%s' to '%s'%n", disc, src, dst);
     }
 
     public static void main(String[] args) {
