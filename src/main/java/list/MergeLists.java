9dd92f7e6e7942beac06c4577b6333a8c9b2e6d5
Reza Shakoori <reza.shakoori@cornell.edu>
2022-06-24 20:14:13 -0700

    Adding merge two sorted linked lists

---
+++ src/main/java/list/MergeLists.java
@@ -0,0 +1,65 @@
+package list;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class MergeLists {
+
+    ListNode mergeSortedLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        ListNode dummy = new ListNode(-99);
+        ListNode tail = dummy;
+        while (true) {
+            if (list1 == null) {
+                tail.next = list2;
+                break;
+            }
+            if (list2 == null) {
+                tail.next = list1;
+                break;
+            }
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+
+    void printLinkedList(ListNode head) {
+        System.out.print("> ");
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println("<");
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+        list1.next.next.next = new ListNode(10);
+
+        ListNode list2 = new ListNode(-1);
+        list2.next = new ListNode(2);
+        list2.next.next = new ListNode(3);
+
+        MergeLists ml = new MergeLists();
+        ListNode merged = ml.mergeSortedLists(list1, list2);
+
+        ml.printLinkedList(merged);
+    }
+
+}
