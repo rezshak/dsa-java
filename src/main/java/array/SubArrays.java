f64f5f30af45d169565307d7eedcd1114dc749ed
Reza Shakoori <reza.shakoori@cornell.edu>
2022-08-13 20:27:29 -0700

    Project tweaks

---
+++ src/main/java/array/SubArrays.java
@@ -0,0 +1,35 @@
+package array;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubArrays {
+
+    static List<int[]> findSubArraysBf(int[] A, int target) {
+        List<int[]> result = new ArrayList<>();
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            int currSum = 0;
+            for (int j = i; j < len; j++) {
+                currSum += A[j];
+                if (currSum == target) {
+                    int[] subarray = new int[j - i + 1];
+                    System.arraycopy(A, i, subarray, 0, subarray.length);
+                    result.add(subarray);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<int[]> subarrays = findSubArraysBf(arr, 9);
+        for (int[] subarray : subarrays) {
+            System.out.println(Arrays.toString(subarray));
+        }
+    }
+
+}
