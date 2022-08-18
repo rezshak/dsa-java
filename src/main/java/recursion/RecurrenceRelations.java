debbc6303c0a922c5b8be0237da3a2a88acaa48f
Reza Shakoori <rshakoori@irhythmtech.com>
2022-08-18 16:49:47 -0700

    Recurrence relations updates

--- src/main/java/recursion/RecurrenceRelations.java
+++ src/main/java/recursion/RecurrenceRelations.java
@@ -2,15 +2,15 @@ package main.java.recursion;
 
 public class RecurrenceRelations {
 
-    // T(n) = T(n - 1) + 1
+    // T(n) = T(n - 1) + 1 => O(n)
     void Test1(int n) {
-        if (n >  0) {
+        if (n > 0) {
             System.out.printf("%d\n", n);
             Test1(n - 1);
         }
     }
 
-    // T(n) = T(n - 1) + n
+    // T(n) = T(n - 1) + n => O(n^2)
     void TestN(int n) {
         if (n > 0) {
             for (int i = 0; i < n; i++) {
@@ -20,7 +20,7 @@ public class RecurrenceRelations {
         }
     }
 
-    // T(n) = T(n - 1) + log n
+    // T(n) = T(n - 1) + log n => O(nlog n)
     void TestLogN(int n) {
         if (n > 0) {
             for (int i = 1; i < n; i *= 2) {
@@ -30,7 +30,11 @@ public class RecurrenceRelations {
         }
     }
 
-    // T(n) = 2T(n - 1) + 1
+    // T(n) = T(n - 1) + n^2 => O(n^3)
+    // T(n) = T(n - 2) + 1  => n/2 O(n) => O(n)
+    // T(n) = T(n - 100) + n => O(n^2)
+
+    // T(n) = 2T(n - 1) + 1 => O(2^n)
     void Test2T(int n) {
         if (n > 0) {
             System.out.printf("%d\n", n);
@@ -39,10 +43,17 @@ public class RecurrenceRelations {
         }
     }
 
+    // T(n) = 3T(n - 1) + 1 	=> O(3^n)
+    // T(n) = 2T(n - 1) + n 	=> O(n2^n)
+
+
+
+
+
     public static void main(String[] args) {
 
         RecurrenceRelations rr = new RecurrenceRelations();
-        rr.Test1(3);
+        rr.Test2T(3);
 
     }
 
