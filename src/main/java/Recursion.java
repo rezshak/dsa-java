502105dbb4c05b88627cbf4c55abb20b5e8c5c41
Reza Shakoori <reza.shakoori@cornell.edu>
2022-08-18 19:24:14 -0700

    Add indirect recursion

---
+++ src/main/java/Recursion.java
@@ -0,0 +1,55 @@
+package main.java;
+
+class Recursion {
+
+    void tailRecursion(int n) {
+        if (n > 0) {
+            System.out.println(n);
+            tailRecursion(n - 1);
+        }
+    }
+
+    void tailLoop(int n) {
+        while (n > 0) {
+            System.out.printf("%d\n", n);
+            n--;
+        }
+    }
+
+    void headRecursion(int n) {
+        if (n > 0) {
+            headRecursion(n - 1);
+            System.out.printf("%d\n", n);
+        }
+    }
+
+    void headLoop(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%d\n", n);
+        }
+    }
+
+    void indirectRecursionA(int n) {
+        if (n > 0) {
+            System.out.printf("%d\n", n);
+            indirectRecursionB(n - 1);
+        }
+    }
+
+    private void indirectRecursionB(int n) {
+        if (n > 1) {
+            System.out.printf("%d\n", n);
+            indirectRecursionA(n / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Recursion rec = new Recursion();
+//        rec.tailRecursion(3);
+//        rec.tailLoop(3);
+//        rec.headRecursion(3);
+//        rec.headLoop(3);
+        rec.indirectRecursionA(16);
+    }
+
+}
