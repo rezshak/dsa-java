8afcd129b69c050ef9bedbdce3cb8b8a59f3f226
Reza Shakoori <rshakoori@irhythmtech.com>
2022-08-26 10:15:49 -0700

    Bubble sort tweak

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -4,10 +4,8 @@ import java.util.Arrays;
 
 class BubbleSort {
 
-    static final int[] ARR0 = {1, 3, 2};
-    static final int[] ARR1 = {8, 6, 2, 4, 5, 8};
-    static final int[] ARR2 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90, 12, 55, 82, 3, 29, 69, 13, 57, 73, 5};
-    static final int[] ARR3 = {23, 7, 29, 79, 12, 41, 57, 72, 45, 82, 51, 10, 40, 85, 72, 13, 50, 90, 100, 6, 39, 100};
+    static final int[] ARR1 = {1, 3, 2};
+    static final int[] ARR2 = {8, 6, 2, 4, 5, 8};
 
     void bubbleSort(int[] A) {
         int lastSortedIndex = A.length - 1;
@@ -17,8 +15,8 @@ class BubbleSort {
             for (int i = 0; i < lastSortedIndex; i++) {
                 if (A[i] > A[i + 1]) {
                     swap(A, i, i + 1);
+                    sorted = false;
                 }
-                sorted = false;
             }
             lastSortedIndex--;
         }
