ce0090c6baf853bcebdc55a8c6e8b9ec5d7bdc76
Reza Shakoori <rshakoori@irhythmtech.com>
2023-08-11 16:55:01 -0700

    Merge sort tweak

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -25,29 +25,21 @@ public class MergeSort {
     void merge(int[] nums, int left, int mid, int right) {
         int len1 = mid - left + 1;
         int len2 = right - mid;
-
         int[] nums1 = new int[len1];
         int[] nums2 = new int[len2];
-
         for (int i = 0; i < len1; i++) {
             nums1[i] = nums[left + i];
         }
-
         for (int j = 0; j < len2; j++) {
             nums2[j] = nums[mid + 1 + j];
         }
-
-        int idx1 = 0, idx2 = 0;
-        int idx = left;
-
+        int idx1 = 0, idx2 = 0, idx = left;
         while (idx1 < len1 && idx2 < len2) {
             nums[idx++] = nums1[idx1] <= nums2[idx2] ? nums1[idx1++] : nums2[idx2++];
         }
-
         while (idx1 < len1) {
             nums[idx++] = nums1[idx1++];
         }
-
         while (idx2 < len2) {
             nums[idx++] = nums2[idx2++];
         }
