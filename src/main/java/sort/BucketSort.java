73d5e963a6986e39de1d6e0cb5a010322f198f39
Reza Shakoori <rshakoori@irhythmtech.com>
2023-09-18 12:03:16 -0700

    Bucket sort tweaks

--- src/main/java/sort/BucketSort.java
+++ src/main/java/sort/BucketSort.java
@@ -1,32 +1,37 @@
 package main.java.sort;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 class BucketSort {
 
     static final int[] ARR1 = {8, 6, 2, 4, 5, 8, 9};
     static final int[] ARR2 = {6, 43, 48, 18, 62, 29, 26, 3, 22, 13, 17, 56, 90};
 
-    void bucketSort(int[] A, int numberOfBuckets) {
+    void bucketSort(int[] nums, int bucketsCount) {
+        var buckets = createBuckets(bucketsCount);
+        for (var n : nums) {
+            var bucketIndex = n / bucketsCount;
+            if (bucketIndex > bucketsCount) bucketIndex = buckets.size() - 1;
+            buckets.get(bucketIndex).add(n);
+        }
         var i = 0;
-        for (var bucket : createBuckets(A, numberOfBuckets)) {
+        for (var bucket : buckets) {
+
             Collections.sort(bucket);
-            for (var item : bucket)
-                A[i++] = item;
+            for (var n : bucket) {
+                nums[i++] = n;
+            }
         }
     }
 
-    List<List<Integer>> createBuckets(int[] A, int numberOfBuckets) {
-        List<List<Integer>> buckets = new ArrayList<>();
-        for (var i = 0; i < numberOfBuckets; i++)
+    private List<List<Integer>> createBuckets(int bucketsCount) {
+        var buckets = new ArrayList<List<Integer>>();
+        for (var i = 0; i < bucketsCount; i++) {
             buckets.add(new ArrayList<>());
-
-        for (var item : A) {
-            int bucketIndex = item / numberOfBuckets;
-            if (bucketIndex > numberOfBuckets) bucketIndex = numberOfBuckets - 1;
-            buckets.get(bucketIndex).add(item);
         }
-
         return buckets;
     }
 
