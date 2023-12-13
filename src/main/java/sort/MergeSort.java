7ca0d2e6eea30df6d2323642d8d55fecb4d0a0a9
Reza Shakoori <rshakoori@irhythmtech.com>
2023-12-13 11:04:47 -0800

    Merge sort alternate impl

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -9,20 +9,47 @@ public class MergeSort {
     private static final int[] ARR3 = {7};
     private static final int[] ARR4 = {};
 
-    void mergeSort(int[] A) {
-        mergeSort(A, 0, A.length - 1);
+    void mergeSort(int[] nums) {
+        if (nums.length < 2) return;
+        var len = nums.length;
+        var mid = len / 2;
+        var nums1 = new int[mid];
+        var nums2 = new int[len - mid];
+        System.arraycopy(nums, 0, nums1, 0, mid);
+        System.arraycopy(nums, mid, nums2, 0, len - mid);
+        mergeSort(nums1);
+        mergeSort(nums2);
+        merge(nums, nums1, nums2);
     }
 
-    void mergeSort(int[] nums, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(nums, left, mid);
-            mergeSort(nums, mid + 1, right);
-            merge(nums, left, mid, right);
+    void merge(int[] nums, int[] nums1, int[] nums2) {
+        int len1 = nums1.length, len2 = nums2.length;
+        int idx = 0, idx1 = 0, idx2 = 0;
+        while (idx1 < len1 && idx2 < len2) {
+            nums[idx++] = nums1[idx1] <= nums2[idx2] ? nums1[idx1++] : nums2[idx2++];
+        }
+        while (idx1 < len1) {
+            nums[idx++] = nums1[idx1++];
+        }
+        while (idx2 < len2) {
+            nums[idx++] = nums2[idx2++];
         }
     }
 
-    void merge(int[] nums, int left, int mid, int right) {
+    void mergeSort2(int[] nums) {
+        mergeSort2(nums, 0, nums.length - 1);
+    }
+
+    void mergeSort2(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort2(nums, left, mid);
+            mergeSort2(nums, mid + 1, right);
+            merge2(nums, left, mid, right);
+        }
+    }
+
+    void merge2(int[] nums, int left, int mid, int right) {
         int len1 = mid - left + 1;
         int len2 = right - mid;
         int[] nums1 = new int[len1];
@@ -47,7 +74,7 @@ public class MergeSort {
 
     public static void main(String... args) {
         MergeSort ms = new MergeSort();
-        ms.mergeSort(ARR2);
+        ms.mergeSort2(ARR2);
         System.out.println(Arrays.toString(ARR2));
     }
 
